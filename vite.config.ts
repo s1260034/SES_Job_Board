6747c9e5cd76cea535c5a9357eb5d6fe2bf33972
Yuki Takarada <52790525+s1260034@users.noreply.github.com>
2025-08-13 11:43:25 +0900

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 @@ .. @@
- // https://vitejs.dev/config/
- export default defineConfig({
-   plugins: [react()],
-+  server: {
-+    host: true,
-+    port: 5173
-+  },
-   optimizeDeps: {
-     exclude: ['lucide-react'],
-   },
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173
+  },
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+})
